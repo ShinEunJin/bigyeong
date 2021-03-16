a123642992d72afff4759e00401269e7f7aac8bf
ShinEunJin <sineun5501@naver.com>
2021-03-16 14:59:21 +0900

    upload basic site

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 import express from "express"
+import { localsMiddleware } from "./middlewares"
+import globalRouter from "./routers/globalRouter"
+import videoRouter from "./routers/videoRouter"
+import routes from "./routes"
 
 const app = express()
+
+app.set("view engine", "pug")
+
+app.use(localsMiddleware)
+
+app.use(routes.home, globalRouter)
+app.use(routes.video, videoRouter)
 
 export default app
---
+++ routes.js
@@ -0,0 +1,12 @@
+const HOME = "/"
+
+const VIDEO = "/video"
+const UPLOAD = "/upload"
+
+const routes = {
+    home: HOME,
+    video: VIDEO,
+    upload: UPLOAD
+}
+
+export default routes
