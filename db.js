7c7cc5c6dc57ff4bd98642ecc8fa3aaf87003cf0
ShinEunJin <sineun5501@naver.com>
2021-03-22 17:47:55 +0900

    mongoose connect and body-parser

---
+++ db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_DEV_URI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
+}).then(() => console.log("✅ Connected to MongoDB"))
+    .catch(err => console.log(`❌ ${err}`))
