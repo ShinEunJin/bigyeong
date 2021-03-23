8dfc645714d1a9f76278c68a5b0d334b991688a5
ShinEunJin <sineun5501@naver.com>
2021-03-23 17:30:21 +0900

    add logout backend

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express"
 import bodyParser from "body-parser"
 import cookieParser from "cookie-parser"
 import routes from "./routes"
-import { auth, login, register } from "./controllers/userController"
+import { auth, login, logout, register } from "./controllers/userController"
 import middleAuth from "./middlewares/middleAuth"
 
 
@@ -15,5 +15,6 @@ app.use(cookieParser())
 app.post(routes.register, register)
 app.post(routes.login, login)
 app.get(routes.auth, middleAuth, auth)
+app.get(routes.logout, middleAuth, logout)
 
 export default app
--- controllers/userController.js
+++ controllers/userController.js
@@ -47,3 +47,12 @@ export const auth = (req, res) => {
         image: req.user.image
     })
 }
+
+export const logout = async (req, res) => {
+    try {
+        await User.findOneAndUpdate({ _id: req.user._id }, { token: "" })
+        return res.status(200).json({ success: true })
+    } catch (error) {
+        return res.json({ success: false, error })
+    }
+}
