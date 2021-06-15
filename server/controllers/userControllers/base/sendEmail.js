b67b4c2b1815c5f7247908c6b5f94f1a2e98507c
ShinEunJin <sineun5501@naver.com>
2021-06-15 16:11:19 +0900

    nodemailer fixed

--- server/controllers/userControllers/base/sendEmail.js
+++ server/controllers/userControllers/base/sendEmail.js
@@ -10,6 +10,9 @@ export const registerEmail = async (req, res) => {
   try {
     let transporter = nodemailer.createTransport({
       service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_ID,
         pass: process.env.EMAIL_PASSWORD,
